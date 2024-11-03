ba46afd000ccd303f2e6c81f42cb8f5c5e37ad5c
L. Thorud <lthorud12@gmail.com>
2024-11-02 22:21:22 -0500

    secure store

--- App.js
+++ App.js
@@ -1,20 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createContext, useReducer, useMemo, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as SecureStore from "expo-secure-store";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { ABeeZee_400Regular, useFonts } from '@expo-google-fonts/abeezee'
+import { Poppins_400Regular, Poppins_600SemiBold, Poppins_900Black } from '@expo-google-fonts/poppins'
+import { AppStack, AuthStack } from "./Navigation";
+
+const AuthContext = createContext();
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    'ABeeZee': ABeeZee_400Regular,
+    'Poppins': Poppins_400Regular,
+    'PoppinsSemi': Poppins_600SemiBold,
+    'PoppinsBlack': Poppins_900Black
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+
+  const [state, dispatch] = useReducer(
+    (prevState, action) => {
+      switch (action.type) {
+        case "RESTORE_TOKEN":
+          return {
+            ...prevState,
+            userToken: action.token,
+            isLoading: false,
+          };
+        case "SIGN_IN":
+          return {
+            ...prevState,
+            isSignout: false,
+            userToken: action.token,
+          };
+        case "SIGN_OUT":
+          return {
+            ...prevState,
+            isSignout: true,
+            userToken: null,
+          };
+      }
+    },
+    {
+      isLoading: true,
+      isSignout: false,
+      userToken: null,
+    }
+  );
+
+  useEffect(() => {
+    const bootstrapAsync = async () => {
+      let userToken;
+
+      try {
+        userToken = await SecureStore.getItemAsync("userToken");
+      } catch (e) {}
+      dispatch({ type: "RESTORE_TOKEN", token: userToken });
+    };
+
+    bootstrapAsync();
+  }, []);
+
+  const authContext = useMemo(
+    () => ({
+      signIn: async (data) => {
+        dispatch({ type: "SIGN_IN", token: "dummy" });
+      },
+      signOut: () => dispatch({ type: "SIGN_OUT" }),
+      signUp: async (data) => {
+        dispatch({ type: "SIGN_IN", token: "dummy" });
+      },
+    }),
+    []
+  );
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AuthContext.Provider value={authContext}>
+        <Stack.Navigator>
+          {state.userToken == null ? (
+            <Stack.Screen name="Auth" component={AuthStack} />
+          ) : (
+              <Stack.Navigator name='App' component={AppStack} />
+          )}
+        </Stack.Navigator>
+      </AuthContext.Provider>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ Navigation.js
@@ -0,0 +1,25 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Welcome from './screens/Auth/Welcome'
+
+import Dashboard from "./screens/Dashboard";
+
+const Stack = createNativeStackNavigator()
+
+
+export const AuthStack = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Welcome" component={Welcome} />
+        </Stack.Navigator>
+    )
+}
+
+export const AppStack = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Dashboard" component={Dashboard} />
+        </Stack.Navigator>
+    )
+}
+
